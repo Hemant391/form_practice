b0575b9ed60fadd22bad2a1c2e4b3aab25e62eb1
Hemant391 <148564538+Hemant391@users.noreply.github.com>
2024-06-12 19:13:10 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React,{useEffect, useState} from 'react'
+
+
+function App() {
+
+  const [fullname,setFullname]=useState('')
+  const [first,setFirst]=useState('')
+  const [last,setLast]=useState('')
+
+  const handlesubmit=(e)=>{
+    e.preventDefault()
+    setFullname(first+' '+last)
+  }
+  
+  return (
+    <div className="App">
+      <h1 >Full Name Display</h1>
+<form onSubmit={handlesubmit}>
+  <p><span>First Name:</span> <span><input value={first} onChange={(e)=>setFirst(e.target.value)} type="text" required/></span></p>
+  <p><span>Last Name:</span> <span><input value={last} onChange={(e)=>setLast(e.target.value)} type="text" required/></span></p>
+  <button type="submit" >Submit</button>
+</form>
+
+    {/* { first.length>0 && last.length>0 && (<p>Full Name: {first+' '+last}</p>)} */}
+    {fullname? (<p>Full name : {fullname}</p>):''}
+    </div>
+  );
+}
+
+export default App;
